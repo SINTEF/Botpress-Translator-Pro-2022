c083311cb9fcf9e5093de39969cb7421daadd7d6
Antoine Pultier <antoine.pultier@sintef.no>
2022-08-10 14:25:26 +0200

    🌈 Pack is now implemented

--- botpress_translator_pro_2022.py
+++ botpress_translator_pro_2022.py
@@ -15,6 +15,8 @@ import json
 import tarfile
 import tempfile
 import openpyxl
+from extract import extract
+from pack import pack
 
 from translate import translate
 
@@ -51,253 +53,25 @@ args.add_argument(
     default="fr",
 )
 
+args.add_argument(
+    '-n',
+    '--new',
+    help='Path to the new chatbot',
+    default='new.tgz'
+)
 
-def extract(bot_path, excel_path, source, target):
+if __name__ == "__main__":
+    args = args.parse_args()
+
     # If the bot path contains an star, find the first file that matches the pattern
+    bot_path = args.bot
     if "*" in bot_path:
         bot_path = glob.glob(bot_path)[0]
         if not bot_path:
             print("No bot found for path " + bot_path)
-            return
+            exit(1)
 
-    print("Loading texts from bot " + bot_path)
-    # Extract the bot to a temporary directory
-    with tempfile.TemporaryDirectory() as temporary_directory:
-        # Extract the .tgz file in python
-        with tarfile.open(bot_path, "r:gz") as tar:
-            tar.extractall(temporary_directory)
-        # Open content-elements/builtin_text.json
-        with open(
-            temporary_directory + "/content-elements/builtin_text.json", "r"
-        ) as f:
-            # read and parse json
-            builtin_texts = json.load(f)
-            # builtin_text is an array of objects containing an id and a formData object
-            entries = [
-                (entry["id"], entry["formData"]["text$en"]) for entry in builtin_texts
-            ]
-
-        # Open content-elements/builtin_card.json
-        with open(
-            temporary_directory + "/content-elements/builtin_card.json", "r"
-        ) as f:
-            # read and parse json
-            builtin_cards = json.load(f)
-            # builtin_card is an array of objects containing an id and a formData object
-            for builtin_card in builtin_cards:
-                entries.append(
-                    (
-                        builtin_card["id"] + ".title",
-                        builtin_card["formData"]["title$en"],
-                    )
-                )
-                if "subtitle$en" in builtin_card["formData"]:
-                    entries.append(
-                        (
-                            builtin_card["id"] + ".subtitle",
-                            builtin_card["formData"]["subtitle$en"],
-                        )
-                    )
-                for index, option in enumerate(builtin_card["formData"]["actions$en"]):
-                    entries.append(
-                        (
-                            builtin_card["id"] + ".actionTitle[" + str(index) + "]",
-                            option["title"],
-                        )
-                    )
-        # Open content-elements/builtin_carousel.json
-        with open(
-            temporary_directory + "/content-elements/builtin_carousel.json", "r"
-        ) as f:
-            # read and parse json
-            builtin_carousels = json.load(f)
-            # builtin_carousel is an array of objects containing an id and a formData object
-            for builtin_carousel in builtin_carousels:
-                for index, item in enumerate(builtin_carousel["formData"]["items$en"]):
-                    entries.append(
-                        (
-                            builtin_carousel["id"] + ".itemTitle[" + str(index) + "]",
-                            item["title"],
-                        )
-                    )
-                    for actionIndex, option in enumerate(item["actions"]):
-                        entries.append(
-                            (
-                                builtin_carousel["id"]
-                                + ".itemActionTitle["
-                                + str(index)
-                                + "]["
-                                + str(actionIndex)
-                                + "]",
-                                option["title"],
-                            )
-                        )
-        # Open content-elements/builtin_image.json
-        with open(
-            temporary_directory + "/content-elements/builtin_image.json", "r"
-        ) as f:
-            # read and parse json
-            builtin_images = json.load(f)
-            # builtin_image is an array of objects containing an id and a formData object
-            for builtin_image in builtin_images:
-                if "title$en" in builtin_image["formData"]:
-                    entries.append(
-                        (
-                            builtin_image["id"] + ".title",
-                            builtin_image["formData"]["title$en"],
-                        )
-                    )
-
-        # Open content-elements/builtin_single-choice.json
-        with open(
-            temporary_directory + "/content-elements/builtin_single-choice.json", "r"
-        ) as f:
-            # read and parse json
-            builtin_single_choices = json.load(f)
-            # builtin_single-choice is an array of objects containing an id and a formData object
-            for builtin_single_choice in builtin_single_choices:
-                entries.append(
-                    (
-                        builtin_single_choice["id"] + ".dropdown",
-                        builtin_single_choice["formData"]["dropdownPlaceholder$en"],
-                    )
-                )
-                if "title$en" in builtin_single_choice["formData"]:
-                    entries.append(
-                        (
-                            builtin_single_choice["id"] + ".title",
-                            builtin_single_choice["formData"]["title$en"],
-                        )
-                    )
-                for index, option in enumerate(
-                    builtin_single_choice["formData"]["choices$en"]
-                ):
-                    entries.append(
-                        (
-                            builtin_single_choice["id"] + ".choice[" + str(index) + "]",
-                            option["title"],
-                        )
-                    )
-        # We don't translate the videos automatically because they most likely need to be changed
-
-        # Open content-elements/dropdown.json
-        with open(temporary_directory + "/content-elements/dropdown.json", "r") as f:
-            # read and parse json
-            dropdowns = json.load(f)
-            # dropdown is an array of objects containing an id and a formData object
-            for dropdown in dropdowns:
-                entries.append(
-                    (
-                        dropdown["id"] + ".message",
-                        dropdown["formData"]["message$en"],
-                    )
-                )
-                entries.append(
-                    (
-                        dropdown["id"] + ".placeholderText",
-                        dropdown["formData"]["placeholderText$en"],
-                    )
-                )
-                for index, option in enumerate(dropdown["formData"]["options$en"]):
-                    entries.append(
-                        (
-                            dropdown["id"] + ".option[" + str(index) + "]",
-                            option["label"],
-                        )
-                    )
-
-        # Translate the text using Google Translate API
-        print("Translating texts...")
-        texts_to_translate = [text for _, text in entries]
-        translated_texts = translate(texts_to_translate, source, target)
-
-        print("Writing Excel file...")
-        # Create a new excel file
-        wb = openpyxl.Workbook()
-        # Create a new sheet
-        ws = wb.active
-        ws.title = "Translations"
-        # Write the entries to the sheet
-        ws.append(["Identifier", "Original English Text", "Translation"])
-        # ws.append(entries)
-        for (id, text) in entries:
-            ws.append([id, text, translated_texts[text]])
-
-        idStyle = openpyxl.styles.NamedStyle(name="id")
-        idStyle.font = openpyxl.styles.Font(
-            bold=True,
-            name="Courier New",
-            size=9,
-        )
-        idStyle.fill = openpyxl.styles.PatternFill(
-            fgColor="EEEEEE",
-            fill_type="solid",
-        )
-        idStyle.alignment = openpyxl.styles.Alignment(
-            wrap_text=True,
-        )
-        headerStyle = openpyxl.styles.NamedStyle(name="header")
-        headerStyle.font = openpyxl.styles.Font(
-            bold=True,
-            size=10,
-            name="Calibri",
-        )
-        headerStyle.fill = openpyxl.styles.PatternFill(
-            fgColor="CCCCCC",
-            fill_type="solid",
-        )
-        originalStyle = openpyxl.styles.NamedStyle(name="original")
-        originalStyle.font = openpyxl.styles.Font(
-            name="Calibri",
-            # Dark brown colour
-            color="663300",
-            italic=True,
-        )
-        originalStyle.alignment = openpyxl.styles.Alignment(
-            vertical="top",
-            wrap_text=True,
-        )
-        translationStyle = openpyxl.styles.NamedStyle(name="translation")
-        translationStyle.font = openpyxl.styles.Font(
-            name="Calibri",
-        )
-        translationStyle.alignment = openpyxl.styles.Alignment(
-            vertical="top",
-            wrap_text=True,
-        )
-        ws["A1"].style = idStyle
-        ws.column_dimensions["A"].width = 20
-        ws.column_dimensions["B"].width = 30
-        ws.column_dimensions["C"].width = 30
-
-        # Lock the cells that shouldn't be edited
-        ws.protection.sheet = True
-
-        # Set headers styles
-        for row in ws["A1":"C1"]:
-            for cell in row:
-                cell.style = headerStyle
-        # Set identifier styles
-        for row in ws.iter_rows(min_row=2, max_row=ws.max_row, min_col=1, max_col=1):
-            for cell in row:
-                cell.style = idStyle
-        # Set original text style
-        for row in ws.iter_rows(min_row=2, max_row=ws.max_row, min_col=2, max_col=2):
-            for cell in row:
-                cell.style = originalStyle
-        # Set translation styles
-        for row in ws.iter_rows(min_row=2, max_row=ws.max_row, min_col=3, max_col=3):
-            for cell in row:
-                cell.style = translationStyle
-                # Unlock the translation rows
-                cell.protection = openpyxl.styles.protection.Protection(locked=False)
-        wb.save(excel_path)
-        print("Excel file created: " + excel_path)
-        print("Done 🥳")
-
-if __name__ == "__main__":
-    args = args.parse_args()
     if args.mode == "extract":
-        extract(args.bot, args.excel, args.source, args.target)
+        extract(bot_path, args.excel, args.source, args.target)
     elif args.mode == "pack":
-        pack(args.bot, args.excel)
+        pack(bot_path, args.excel, args.new)
---
+++ pack.py
@@ -0,0 +1,201 @@
+import json
+import tarfile
+import tempfile
+import openpyxl
+import os
+
+def pack(bot_path, excel_path, new_path):
+    print("Load translations from excel " + excel_path)
+    wb = openpyxl.load_workbook(excel_path)
+    ws = wb.active
+
+    # Create a dictionary of translations, the key is the path
+    # The value is a tuple that contains the english and the translation
+    translations=dict()
+
+    # Build the dictionary
+    for row in list(ws.rows)[1:]:
+        [ path, english, translation ] = [ cell.value for cell in row ]
+        translations[path] = (english, translation)
+
+    def get_translation(path, expected_existing_english):
+        if not path in translations:
+            raise Exception("No translation found for " + str(path)) 
+        (english, translation) = translations[path]
+        if expected_existing_english and english != expected_existing_english:
+            raise Exception("Expected " + str(expected_existing_english) + " but found " + str(english))
+        
+        return translation
+
+    print("Patching the bot " + bot_path)
+    # Extract the bot to a temporary directory
+    with tempfile.TemporaryDirectory() as temporary_directory:
+        # Extract the .tgz file in python
+        with tarfile.open(bot_path, "r:gz") as tar:
+            tar.extractall(temporary_directory)
+
+        # Open content-elements/builtin_text.json
+        with open(
+            temporary_directory + "/content-elements/builtin_text.json", "r"
+        ) as f:
+            # read and parse json
+            builtin_texts = json.load(f)
+            # builtin_text is an array of objects containing an id and a formData object
+            for builtin_text in builtin_texts:
+                translation = get_translation(builtin_text["id"], builtin_text["formData"]["text$en"])
+                # We keep the $en field because it seems that our version of botpress
+                # does not support other languages
+                builtin_text["formData"]["text$en"] = translation
+
+            translated_builtin_texts = json.dumps(builtin_texts, indent=2, ensure_ascii=False)
+
+        with open(
+            temporary_directory + "/content-elements/builtin_text.json", "w"
+        ) as f:
+            f.write(translated_builtin_texts)
+
+        # Open content-elements/builtin_card.json
+        with open(
+            temporary_directory + "/content-elements/builtin_card.json", "r"
+        ) as f:
+            # read and parse json
+            builtin_cards = json.load(f)
+            # builtin_card is an array of objects containing an id and a formData object
+            for builtin_card in builtin_cards:
+
+                translation = get_translation(builtin_card["id"]+".title", builtin_card["formData"]["title$en"])
+                builtin_card["formData"]["title$en"] = translation
+
+                if "subtitle$en" in builtin_card["formData"]:
+                    translation = get_translation(builtin_card["id"]+".subtitle", builtin_card["formData"]["subtitle$en"])
+                    builtin_card["formData"]["subtitle$en"] = translation
+
+                for index, option in enumerate(builtin_card["formData"]["actions$en"]):
+                    translation = get_translation(builtin_card["id"] + ".actionTitle[" + str(index) + "]", option["title"])
+                    builtin_card["formData"]["actions$en"][index]["title"] = translation
+                    if "text" in option:
+                        translation = get_translation(builtin_card["id"] + ".actionText[" + str(index) + "]", option["text"])
+                        builtin_card["formData"]["actions$en"][index]["text"] = translation
+
+            translated_builtin_cards = json.dumps(builtin_cards, indent=2, ensure_ascii=False)
+        
+        with open(
+            temporary_directory + "/content-elements/builtin_card.json", "w"
+        ) as f:
+            f.write(translated_builtin_cards)
+
+        # Open content-elements/builtin_carousel.json
+        with open(
+            temporary_directory + "/content-elements/builtin_carousel.json", "r"
+        ) as f:
+            # read and parse json
+            builtin_carousels = json.load(f)
+            # builtin_carousel is an array of objects containing an id and a formData object
+            for builtin_carousel in builtin_carousels:
+                for index, option in enumerate(builtin_carousel["formData"]["items$en"]):
+                    translation = get_translation(builtin_carousel["id"] + ".itemTitle[" + str(index) + "]", option["title"])
+                    builtin_carousel["formData"]["items$en"][index]["title"] = translation
+                    for actionIndex, option in enumerate(option["actions"]):
+                        translation = get_translation(builtin_carousel["id"] + ".itemActionTitle[" + str(index) + "][" + str(actionIndex) + "]", option["title"])
+                        builtin_carousel["formData"]["items$en"][index]["actions"][actionIndex]["title"] = translation
+
+            translated_builtin_carousels = json.dumps(builtin_carousels, indent=2, ensure_ascii=False)
+
+        with open(
+            temporary_directory + "/content-elements/builtin_carousel.json", "w"
+        ) as f:
+            f.write(translated_builtin_carousels)
+
+        # Open content-elements/builtin_image.json
+        with open(
+            temporary_directory + "/content-elements/builtin_image.json", "r"
+        ) as f:
+            # read and parse json
+            builtin_images = json.load(f)
+            # builtin_image is an array of objects containing an id and a formData object
+            for builtin_image in builtin_images:
+                if "title$en" in builtin_image["formData"]:
+                    translation = get_translation(builtin_image["id"]+".title", builtin_image["formData"]["title$en"])
+                    builtin_image["formData"]["title$en"] = translation
+
+            translated_builtin_images = json.dumps(builtin_images, indent=2, ensure_ascii=False)
+
+        with open(
+            temporary_directory + "/content-elements/builtin_image.json", "w"
+        ) as f:
+            f.write(translated_builtin_images)
+
+        # Open content-elements/builtin_single-choice.json
+        with open(
+            temporary_directory + "/content-elements/builtin_single-choice.json", "r"
+        ) as f:
+            # read and parse json
+            builtin_single_choices = json.load(f)
+            # builtin_single_choice is an array of objects containing an id and a formData object
+            for builtin_single_choice in builtin_single_choices:
+                translation = get_translation(builtin_single_choice["id"]+".dropdown", builtin_single_choice["formData"]["dropdownPlaceholder$en"])
+                builtin_single_choice["formData"]["dropdownPlaceholder$en"] = translation
+
+                if "text$en" in builtin_single_choice["formData"]:
+                    translation = get_translation(builtin_single_choice["id"]+".text", builtin_single_choice["formData"]["text$en"])
+                    builtin_single_choice["formData"]["text$en"] = translation
+
+                for index, option in enumerate(builtin_single_choice["formData"]["choices$en"]):
+                    translation = get_translation(builtin_single_choice["id"] + ".choice[" + str(index) + "]", option["title"])
+                    builtin_single_choice["formData"]["choices$en"][index]["title"] = translation
+
+            translated_builtin_single_choices = json.dumps(builtin_single_choices, indent=2, ensure_ascii=False)
+
+        with open(
+            temporary_directory + "/content-elements/builtin_single-choice.json", "w"
+        ) as f:
+            f.write(translated_builtin_single_choices)
+
+        # Open content-elements/dropdown.json
+        with open(
+            temporary_directory + "/content-elements/dropdown.json", "r"
+        ) as f:
+            # read and parse json
+            dropdowns = json.load(f)
+            # dropdown is an array of objects containing an id and a formData object
+            for dropdown in dropdowns:
+                translation = get_translation(dropdown["id"]+".message", dropdown["formData"]["message$en"])
+                dropdown["formData"]["message$en"] = translation
+
+                translation = get_translation(dropdown["id"]+".placeholderText", dropdown["formData"]["placeholderText$en"])
+                dropdown["formData"]["placeholderText$en"] = translation
+
+                for index, option in enumerate(dropdown["formData"]["options$en"]):
+                    translation = get_translation(dropdown["id"] + ".option[" + str(index) + "]", option["label"])
+                    dropdown["formData"]["options$en"][index]["label"] = translation
+
+            translated_dropdowns = json.dumps(dropdowns, indent=2, ensure_ascii=False)
+
+        with open(
+            temporary_directory + "/content-elements/dropdown.json", "w"
+        ) as f:
+            f.write(translated_dropdowns)
+
+        # This is disabled as translations are not available in our botpress version
+        # # Open bot.config.json
+        # with open(
+        #     temporary_directory + "/bot.config.json", "r"
+        # ) as f:
+        #     # read and parse json
+        #     bot_config = json.load(f)
+        #     bot_config["languages"] = ["no", "en", "fr"]
+        #     bot_config["defaultLanguage"] = "no"
+
+        #     translated_bot_config = json.dumps(bot_config, indent=2, ensure_ascii=False)
+
+        # with open(
+        #     temporary_directory + "/bot.config.json", "w"
+        # ) as f:
+        #     f.write(translated_bot_config)
+
+        print("Writing the new bot to " + new_path)
+        # Create a .tgz file of the temporary directory
+        with tarfile.open(new_path, "w:gz") as tar:
+            for name in os.listdir(temporary_directory):
+                tar.add(temporary_directory + "/" + name, arcname=name)
+
